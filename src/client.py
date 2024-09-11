a45471710ef423d6f62b40685330045db46358f0
Poom Wongvilart <99780985+Derpyderp157@users.noreply.github.com>
2024-09-11 19:43:29 -0400

    Started implimenting client sockets and server sockets

--- src/client.py
+++ src/client.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
+import socket
 
 # droplatch client
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 9999))
+s.send(str.encode('hello'))
--- src/server.py
+++ src/server.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
+import socket
 
 # droplatch server
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', 9999))
+serversocket.listen(2)
+
+while True:
+    connection, address = serversocket.accept()
+    buf = connection.recv(64)
+    if len(buf) > 0:
+        print(buf)
+        break
+
